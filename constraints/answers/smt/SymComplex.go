2a0a73e841a319efd4186ce173894854cfd359d4
Vladislav Feofilaktov <vfeofilaktov@ptsecurity.com>
2024-10-13 00:05:43 +0300

    Task-1: Disable float assertions

--- constraints/answers/smt/SymComplex.go
+++ constraints/answers/smt/SymComplex.go
@@ -11,12 +11,6 @@ func (sCtx *SymContext) NewComplexConst(name string) SymComplex {
 	reConst := sCtx.Ctx.Const(name+".real", sCtx.Ctx.FloatSort(11, 53)).(z3.Float)
 	imConst := sCtx.Ctx.Const(name+".imag", sCtx.Ctx.FloatSort(11, 53)).(z3.Float)
 
-	sCtx.Solver.Assert(reConst.IsZero().And(reConst.IsPositive()).Not())
-	sCtx.Solver.Assert(imConst.IsZero().And(imConst.IsPositive()).Not())
-
-	sCtx.Solver.Assert(reConst.IsNaN().Not())
-	sCtx.Solver.Assert(imConst.IsNaN().Not())
-
 	return SymComplex{
 		reConst,
 		imConst,
